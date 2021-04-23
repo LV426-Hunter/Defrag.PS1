dbde64a901a23ddc81405f3a288f589a9a3bdeed
LV426-Hunter <83075877+LV426-Hunter@users.noreply.github.com>
2021-04-23 16:27:28 -0400

    Create Defrag.ps1

---
+++ Defrag.ps1
@@ -0,0 +1,4 @@
+defrag <volumes> | /c | /e <volumes>    [/h] [/m [n]| [/u] [v]]
+defrag <volumes> | /c | /e <volumes> /a [/h] [/m [n]| [/u] [v]]
+defrag <volumes> | /c | /e <volumes> /x [/h] [/m [n]| [/u] [v]]
+defrag <volume> [<parameters>]
